6423ad81f6dd3e042214df374df8a4a664312dd1
William Lima <33525547+willy1989cv@users.noreply.github.com>
2024-08-30 12:07:12 -0100

    create datasets page (#26)

--- frontend/src/components/_shared/Layout.tsx
+++ frontend/src/components/_shared/Layout.tsx
@@ -2,11 +2,26 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import { Toaster } from "@/components/ui/toaster";
-export default function Layout({ children }: { children?: React.ReactNode }) {
+export default function Layout({
+  backgroundEffect,
+  children,
+}: {
+  backgroundEffect?: boolean;
+  children?: React.ReactNode;
+}) {
   return (
     <div className="layout-default relative">
-      <Header />
-      <main className="min-h-[calc(100vh-89px)]">{children}</main>
+      <Header backgroundColor={backgroundEffect ? "#E3F9ED" : "white"} />
+      <main
+        className="min-h-[calc(100vh-89px)]"
+        style={{
+          background: backgroundEffect
+            ? "linear-gradient(180deg, #E3F9ED 0%, rgba(255, 255, 255, 0) 61.27%)"
+            : "",
+        }}
+      >
+        {children}
+      </main>
       <Toaster />
       <Footer />
     </div>
