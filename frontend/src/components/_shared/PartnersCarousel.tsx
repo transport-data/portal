6687808b72dc8ba0a4f89318faeb5c692106e4f5
William Lima <willy.lima89@gmail.com>
2024-09-01 22:57:59 -0100

    add RWD

--- frontend/src/components/_shared/PartnersCarousel.tsx
+++ frontend/src/components/_shared/PartnersCarousel.tsx
@@ -1,43 +1,44 @@
 import { chunkArray } from "@lib/utils";
 import { useEffect, useState } from "react";
 
+const images = [
+  "/images/logos/partners/giz-colored.svg",
+  "/images/logos/partners/ifeu-colored.svg",
+  "/images/logos/partners/oxford-colored.svg",
+  "/images/logos/partners/ricardo-colored.svg",
+  "/images/logos/partners/kfw-colored.svg",
+  "/images/logos/partners/unece-colored.svg",
+  "/images/logos/partners/caf-colored.svg",
+  "/images/logos/partners/adb-colored.svg",
+  "/images/logos/partners/ebrd-colored.svg",
+  "/images/logos/partners/solcat-colored.svg",
+  "/images/logos/partners/fia-colored.svg",
+  "/images/logos/partners/irf-colored.svg",
+  "/images/logos/partners/chalmers-colored.svg",
+  "/images/logos/partners/fdm-colored.svg",
+  "/images/logos/partners/iiasa-colored.svg",
+  "/images/logos/partners/sei-colored.svg",
+  "/images/logos/partners/joint-colored.svg",
+  "/images/logos/partners/uc-davis-colored.svg",
+  "/images/logos/partners/wri-colored.svg",
+  "/images/logos/partners/itdp-colored.svg",
+  "/images/logos/partners/mobilise-colored.svg",
+  "/images/logos/partners/ccg-colored.svg",
+  "/images/logos/partners/sumc-colored.svg",
+  "/images/logos/partners/gnpt-colored.svg",
+  "/images/logos/partners/tumi-colored.svg",
+  "/images/logos/partners/loughbrough-colored.svg",
+  "/images/logos/partners/itf-colored.svg",
+  "/images/logos/partners/icct-colored.svg",
+  "/images/logos/partners/the-world-bank-colored.svg",
+  "/images/logos/partners/cgep-colored.svg",
+];
+
 const Carousel = () => {
   const [currentGroup, setCurrentGroup] = useState(0);
+  const [chunkSize, setChunkSize] = useState(2);
 
-  const images = [
-    "/images/logos/partners/giz-colored.svg",
-    "/images/logos/partners/ifeu-colored.svg",
-    "/images/logos/partners/oxford-colored.svg",
-    "/images/logos/partners/ricardo-colored.svg",
-    "/images/logos/partners/kfw-colored.svg",
-    "/images/logos/partners/unece-colored.svg",
-    "/images/logos/partners/caf-colored.svg",
-    "/images/logos/partners/adb-colored.svg",
-    "/images/logos/partners/ebrd-colored.svg",
-    "/images/logos/partners/solcat-colored.svg",
-    "/images/logos/partners/fia-colored.svg",
-    "/images/logos/partners/irf-colored.svg",
-    "/images/logos/partners/chalmers-colored.svg",
-    "/images/logos/partners/fdm-colored.svg",
-    "/images/logos/partners/iiasa-colored.svg",
-    "/images/logos/partners/sei-colored.svg",
-    "/images/logos/partners/joint-colored.svg",
-    "/images/logos/partners/uc-davis-colored.svg",
-    "/images/logos/partners/wri-colored.svg",
-    "/images/logos/partners/itdp-colored.svg",
-    "/images/logos/partners/mobilise-colored.svg",
-    "/images/logos/partners/ccg-colored.svg",
-    "/images/logos/partners/sumc-colored.svg",
-    "/images/logos/partners/gnpt-colored.svg",
-    "/images/logos/partners/tumi-colored.svg",
-    "/images/logos/partners/loughbrough-colored.svg",
-    "/images/logos/partners/itf-colored.svg",
-    "/images/logos/partners/icct-colored.svg",
-    "/images/logos/partners/the-world-bank-colored.svg",
-    "/images/logos/partners/cgep-colored.svg",
-  ];
-
-  const groups = chunkArray(images, 6);
+  const groups = chunkArray(images, chunkSize);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -46,8 +47,23 @@ const Carousel = () => {
     return () => clearInterval(interval);
   }, [groups.length]);
 
+  useEffect(() => {
+    const updateChunkSize = () => {
+      if (window.innerWidth >= 1024) {
+        setChunkSize(6);
+      } else if (window.innerWidth >= 768) {
+        setChunkSize(3);
+      } else {
+        setChunkSize(1);
+      }
+    };
+    updateChunkSize();
+    window.addEventListener("resize", updateChunkSize);
+    return () => window.removeEventListener("resize", updateChunkSize);
+  }, []);
+
   return (
-    <div className="container relative h-[56px] w-full">
+    <div className="container relative h-[100px] w-full md:h-[56px]">
       {groups.map((group: any, index: number) => (
         <div
           key={index}
@@ -57,13 +73,13 @@ const Carousel = () => {
               : "pointer-events-none opacity-0"
           }`}
         >
-          <div className="flex h-full w-full items-center justify-between justify-between gap-8 overflow-hidden opacity-[0.75] lg:gap-0">
+          <div className="flex h-full w-full items-center justify-center gap-8 overflow-hidden opacity-[0.75] md:justify-between lg:gap-0">
             {group.map((image: any, imgIndex: number) => (
               <img
                 key={imgIndex}
                 src={image}
                 alt={`Partner logo ${imgIndex}`}
-                className=" object-contain grayscale"
+                className="object-contain grayscale"
               />
             ))}
           </div>
