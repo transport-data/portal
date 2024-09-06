df28cd20472d7a10d8b174028422e45ec0931479
Luccas Mateus <luccasmmg@gmail.com>
2024-09-06 10:28:59 -0300

    Fixes on dataset creation

--- frontend/src/components/dataset/form/FileUploader.tsx
+++ frontend/src/components/dataset/form/FileUploader.tsx
@@ -75,6 +75,7 @@ export function FileUploader({
         data: files[0],
       });
     }
+    setIsUploading(true);
     upload();
   }
 
@@ -85,7 +86,25 @@ export function FileUploader({
         <div className="flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600">
           <div className="flex flex-col items-center justify-center pb-6 pt-5">
             {uploading ? (
-              <div className="loader mb-4 h-12 w-12 rounded-full border-4 border-t-4 border-gray-200 ease-linear"></div>
+              <div className="flex flex-col items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="h-8 w-8 animate-spin text-accent"
+                >
+                  <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+                </svg>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  Loading...
+                </p>
+              </div>
             ) : (
               <>
                 <svg
