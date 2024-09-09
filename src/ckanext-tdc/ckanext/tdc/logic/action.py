78a189b75e2da1877c82c59a206fc341a5cc1bc0
Demenech <joaommdemenech@gmail.com>
2024-09-09 00:06:19 -0300

    feat: implement all fields for resources and datasets

---
+++ src/ckanext-tdc/ckanext/tdc/logic/action.py
@@ -0,0 +1,105 @@
+from ckan.plugins import toolkit as tk
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def _fix_topics_field(data_dict):
+    groups = data_dict.get("groups", None)
+    if groups is not None and len(groups) > 0:
+        def is_topic(group):
+            group_type = group.get("type", None)
+            return group_type is None or group_type == "topic" or group_type == "group"
+
+        topics = list(filter(lambda x: is_topic(x), groups))
+        topic_names = [x.get("name") for x in topics]
+        data_dict["topics"] = topic_names
+    else:
+        data_dict["topics"] = []
+
+
+def _fix_geographies_field(data_dict):
+    """
+    When "geographies" field is provided, add dataset
+    to geographies and regions groups
+    """
+    geography_names = data_dict.get("geographies", None)
+
+    if geography_names is not None and len(geography_names) > 0:
+        countries = []
+        regions = []
+
+        priviliged_context = {"ignore_auth": True}
+
+        group_list_action = tk.get_action("group_list")
+        group_list_data_dict = {
+                "type": "geography",
+                "groups": geography_names,
+                "include_extras": True,
+                "all_fields": True,
+                "include_groups": True
+                }
+        group_list = group_list_action(priviliged_context, group_list_data_dict)
+
+        for group in group_list:
+            geography_type = group.get("geography_type")
+            if geography_type == "country":
+                group_name = group.get("name")
+                parent_groups = group.get("groups")
+                region_names = list([x.get("name") for x in parent_groups])
+
+                countries.append(group_name)
+                regions.extend(region_names)
+
+        countries = list(set(countries))
+        regions = list(set(regions))
+
+        geography_groups = [{"name": x, "type": "geography"} for x in countries + regions]
+        groups = data_dict.get("groups", [])
+        groups += geography_groups
+        data_dict["groups"] = groups
+        data_dict["geographies"] = countries
+        data_dict["regions"] = regions
+
+
+@tk.chained_action
+def package_create(up_func, context, data_dict):
+    _fix_geographies_field(data_dict)
+    _fix_topics_field(data_dict)
+    result = up_func(context, data_dict)
+    return result
+
+
+@tk.chained_action
+def package_update(up_func, context, data_dict):
+    _fix_geographies_field(data_dict)
+    _fix_topics_field(data_dict)
+    result = up_func(context, data_dict)
+    return result
+
+
+@tk.chained_action
+def package_patch(up_func, context, data_dict):
+    _fix_geographies_field(data_dict)
+    _fix_topics_field(data_dict)
+    result = up_func(context, data_dict)
+    return result
+
+
+@tk.chained_action
+@tk.side_effect_free
+def package_search(up_func, context, data_dict):
+    include_archived_param = data_dict.get("include_archived", "false")
+    include_archived = tk.asbool(include_archived_param)
+
+    if not include_archived:
+        if "fq" not in data_dict:
+            data_dict["fq"] = ""
+
+        data_dict["fq"] += " -is_archived:(true)"
+
+    if "include_archived" in data_dict:
+        del data_dict["include_archived"]
+
+    result = up_func(context, data_dict)
+    return result
--- src/ckanext-tdc/ckanext/tdc/plugin.py
+++ src/ckanext-tdc/ckanext/tdc/plugin.py
@@ -1,9 +1,17 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+import ckanext.tdc.logic.action as action
+
+import json
+import logging
+log = logging.getLogger(__name__)
+
+
 class TdcPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    # plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IPackageController, inherit=True)
 
     # IConfigurer
 
@@ -11,3 +19,33 @@ class TdcPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'tdc')
+
+    # IActions
+
+    def get_actions(self):
+        return {
+                "package_create": action.package_create,
+                "package_update": action.package_update,
+                "package_patch": action.package_patch,
+                "package_search": action.package_search
+                }
+
+    # IPackageController
+
+    def before_dataset_index(self, data_dict):
+        # This is a fix so that solr stores a list
+        # instead of a string for multivalued fields
+        multi_value_extra_fields = [
+                "topics",
+                "geographies",
+                "regions",
+                "sectors",
+                "modes",
+                "services"]
+        for field in multi_value_extra_fields:
+            value = data_dict.get(field, None)
+            if value is not None and isinstance(value, str):
+                new_value = json.loads(value)
+                if isinstance(new_value, list):
+                    data_dict[field] = new_value
+        return data_dict
