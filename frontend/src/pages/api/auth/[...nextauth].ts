feb8f6ae4cdd054e4d2e041d815ad86b0811fbd9
William Lima <willy.lima89@gmail.com>
2024-09-10 10:57:03 -0100

    setup githup authentication

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,28 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/server/auth";
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
 
-export default NextAuth(authOptions);
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  // Optional callbacks for customization
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true; // Allows the sign-in
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl; // Redirect to the homepage after sign-in
+    },
+    async session({ session, token, user }) {
+      return session; // Pass session information to the client
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token; // Customize JWT token if needed
+    },
+  },
+});
