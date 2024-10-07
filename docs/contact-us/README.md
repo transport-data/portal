c60a6ef192ebfac667418d0f8eae729cd217d653
William Lima <willy.lima89@gmail.com>
2024-10-06 23:14:39 -0100

    ContactUs Page

---
+++ docs/contact-us/README.md
@@ -0,0 +1,8 @@
+# Contact Us Page
+
+This is an informational page, aimed at directing the user to the appropriate contact channel.
+
+- Email contact for general inquiries
+- GitHub Issues Board channel for data-related issues
+
+![Contact Us Page](contact-us.png)
--- frontend/src/components/_shared/Layout.tsx
+++ frontend/src/components/_shared/Layout.tsx
@@ -3,21 +3,25 @@ import Footer from "./Footer";
 import Header from "./Header";
 import { Toaster } from "@/components/ui/toaster";
 export default function Layout({
+  className = "",
   backgroundEffect,
   children,
   effectSize = "61.27%",
+  fullScreen = true,
 }: {
+  className?: string;
   backgroundEffect?: boolean;
   effectSize?: string;
+  fullScreen?: boolean;
   children?: React.ReactNode;
 }) {
   const backgroundGradient = `linear-gradient(180deg, #E3F9ED 0%, rgba(255, 255, 255, 0) ${effectSize})`;
 
   return (
-    <div className="layout-default relative">
+    <div className={`layout-default relative ${className}`}>
       <Header backgroundColor={backgroundEffect ? "#E3F9ED" : "white"} />
       <main
-        className="min-h-[calc(100vh-89px)]"
+        className={`${fullScreen ? "lg:min-h-[calc(100vh-89px)]" : ""}`}
         style={{
           background: backgroundEffect ? backgroundGradient : "",
         }}
