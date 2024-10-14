1d00a1e5e5eec1ff2ce41c2e78786af996b98dbe
Luccas Mateus <Luccasmmg@gmail.com>
2024-10-14 20:19:23 -0300

    Fix upload again (#155)

--- frontend/src/components/dataset/form/Uploads.tsx
+++ frontend/src/components/dataset/form/Uploads.tsx
@@ -63,13 +63,14 @@ export function UploadsForm() {
               id="files-upload"
               onUploadSuccess={(response: UploadResult) => {
                 let url = response.successful[0]?.uploadURL as string;
-                //get last and second to last items in url, last is going to be the name and second to last is going to be the resourceId
+                console.log('RESPONSE', response.successful[0])
                 const urlParts = url.split("/");
                 const resourceId = urlParts[urlParts.length - 2];
+                const fileName = urlParts[urlParts.length - 1];
                 url = `${env.NEXT_PUBLIC_CKAN_URL}/dataset/${form.getValues(
                   "id"
                 )}/resource/${resourceId}/${
-                  response.successful[0]?.name ?? ""
+                  fileName ?? ""
                 }`;
                 append({
                   id: resourceId,
@@ -146,10 +147,11 @@ export function UploadsForm() {
                 //get last and second to last items in url, last is going to be the name and second to last is going to be the resourceId
                 const urlParts = url.split("/");
                 const resourceId = urlParts[urlParts.length - 2];
+                const fileName = urlParts[urlParts.length - 1];
                 url = `${env.NEXT_PUBLIC_CKAN_URL}/dataset/${form.getValues(
                   "id"
                 )}/resource/${resourceId}/${
-                  response.successful[0]?.name ?? ""
+                  fileName ?? ""
                 }`;
                 append({
                   id: resourceId,
