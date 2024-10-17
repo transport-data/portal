5f1e03f7f36cd905e288e68ef845fd45d3cce967
Luccas Mateus <luccasmmg@gmail.com>
2024-10-17 08:18:05 -0300

    Mv resource_type

--- frontend/src/interfaces/ckan/dataset.interface.ts
+++ frontend/src/interfaces/ckan/dataset.interface.ts
@@ -3,7 +3,6 @@ import {
   Dataset as CKANDataset,
   Group,
   Organization,
-  Resource,
   Tag,
 } from "@portaljs/ckan";
 
@@ -72,4 +71,27 @@ export interface Dataset {
   geographies?: string[];
   regions?: string[];
   contributors: string[];
+}
+
+export interface Resource {
+    cache_last_updated?: string;
+    cache_url?: string;
+    created?: string;
+    datastore_active?: boolean;
+    description?: string;
+    format?: string;
+    hash?: string;
+    id: string;
+    last_modified?: string;
+    metadata_modified?: string;
+    mimetype?: string;
+    mimetype_inner?: string;
+    name?: string;
+    package_id?: string;
+    position?: number;
+    resource_type: 'data' | 'documentation';
+    size?: number;
+    state?: "active" | "inactive" | "deleted";
+    url?: string;
+    url_type?: string;
 }
