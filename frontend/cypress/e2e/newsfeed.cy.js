e8b226af3b3236c755eb3ef2d2c2565480d268af
William Lima <willy.lima89@gmail.com>
2024-11-05 23:28:49 -0100

    add tests

--- frontend/cypress/e2e/newsfeed.cy.js
+++ frontend/cypress/e2e/newsfeed.cy.js
@@ -1,33 +1,85 @@
 const ckanUserName = Cypress.env("CKAN_USERNAME");
 const ckanUserPassword = Cypress.env("CKAN_PASSWORD");
+const datasetSuffix = Cypress.env("DATASET_NAME_SUFFIX");
 
-const uuid = () => Math.random().toString(36).slice(2) + "-test";
+const uuid = () => crypto.randomUUID();
 
 const org = `${uuid()}${Cypress.env("ORG_NAME_SUFFIX")}`;
+const sample_org = `${uuid()}${Cypress.env("ORG_NAME_SUFFIX")}`;
+const datasetTitle = `${uuid()}${datasetSuffix}`;
+const datasetName = `${uuid()}${datasetSuffix}`;
+const notes = `${uuid()}${datasetSuffix}`;
+const tags = ["tag1", "tag2", "tag3"];
+const geographies = ["afg"];
+const random = Math.random() * 100;
+const services = ["passenger"];
+const modes = ["heavy-rail"];
+const sectors = ["active-mobility"];
+const frequency = "annually";
+const temporal_coverage_start = new Date(1990, 1, 1).toISOString();
+const temporal_coverage_end = new Date(2005, 1, 1).toISOString();
+const tdc_category =
+  random < 33
+    ? "public"
+    : random > 33 && random < 66
+    ? "tdc_formatted"
+    : "tdc_harmonized";
+
 
 describe("newsfeed tests", () => {
+
+  before(function () {
+    cy.createOrganizationViaAPI({ title: org, name: sample_org });
+    cy.createDatasetViaAPI({
+      name: datasetName,
+      title: datasetTitle,
+      tag_string: tags,
+      owner_org: sample_org,
+      notes: notes,
+      geographies: geographies,
+      tdc_category: tdc_category,
+      temporal_coverage_start: temporal_coverage_start,
+      temporal_coverage_end: temporal_coverage_end,
+      is_archived: false,
+      sectors: sectors,
+      modes: modes,
+      services: services,
+      frequency: frequency,
+    });
+  });
+
+
   beforeEach(function () {
     cy.login(ckanUserName, ckanUserPassword);
   });
 
-  it("Should see the activities", () => {
 
-    cy.visit("/dashboard/organizations/create");
-    //get input with name=title
-    cy.get("input[name=title]").type(org);
-    //check if input with name url has the content of "test-team"
-    cy.get("input[name=name]").should("have.value", org);
-    cy.get("textarea[name=description]").type("Test description");
-    cy.get("button[type=submit]").click();
-
-    cy.visit("dashboard/newsfeed");
-    cy.contains(`You created a new organization ${org}`)
-
-    cy.visit(`/dashboard/organizations/${org}/edit`)
-    cy.contains('button', 'Delete Organization').click();
-    cy.get("button#confirmDelete").click();
-
-    cy.visit("dashboard/newsfeed");
-    cy.contains(`You deleted the organization ${org}`)
+  it("should list orgs created by user", ()=>{
+    cy.visit("/dashboard/newsfeed");
+    cy.get(".selectable-items-list").contains("Organizations").click();
+    cy.get('p.text-base').contains(`You created a new organization ${org}`);
   });
+
+  it("should list dataset created by user", ()=>{
+    cy.visit("/dashboard/newsfeed");
+    cy.get(".selectable-items-list").contains("Datasets").click();
+    cy.get('p.text-base').contains(`You created a new dataset ${datasetTitle}`);
+  });
+
+  it("should list dataset deleted by user", ()=>{
+    cy.deleteDatasetAPI(datasetName);
+    cy.visit("/dashboard/newsfeed");
+    cy.get(".selectable-items-list").contains("Datasets").click();
+    cy.get('p.text-base').contains(`You deleted the dataset ${datasetTitle}`);
+  });
+
+  it("should list organization deleted by user", ()=>{
+    cy.deleteOrganizationAPI(sample_org);
+    cy.visit("/dashboard/newsfeed");
+    cy.get(".selectable-items-list").contains("Organizations").click();
+    cy.get('p.text-base').contains(`You deleted the organization ${org}`);
+  });
+
+  
+
 });
