4b954a23e3c1ae8a1544ffb27ddb02a8d8bf7a86
Luccas Mateus <luccasmmg@gmail.com>
2024-11-22 13:44:35 -0300

    Add caching for images

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,12 +8,34 @@ await import("./src/env.mjs");
 const config = {
   images: {
     unoptimized: true,
-    loader: 'custom',
-    loaderFile: './image-loader.js',
-    domains: ["portal.transport-data.org", "ckan.tdc.dev.datopian.com", "ckan.tdc.prod.datopian.com"],
+    loader: "custom",
+    loaderFile: "./image-loader.js",
+    domains: [
+      "portal.transport-data.org",
+      "ckan.tdc.dev.datopian.com",
+      "ckan.tdc.prod.datopian.com",
+    ],
   },
   reactStrictMode: true,
   output: "standalone",
+  async headers() {
+    if (process.env.NODE_ENV !== "production") {
+      return [];
+    }
+
+    return [
+      {
+        source: "/:all*(css|js|gif|svg|jpg|jpeg|png|woff|woff2)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default config;
