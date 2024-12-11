1c801f35146f7767cd92633451ad052af1b83ee6
Gutts-n <57202549+Gutts-n@users.noreply.github.com>
2024-12-11 21:16:40 +0000

    [xs][frontend][fix] Fix to r2 error

--- frontend/src/server/r2.ts
+++ frontend/src/server/r2.ts
@@ -3,7 +3,10 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 const R2 = new S3Client({
   region: "auto",
-  endpoint: env.R2_ACCESS_KEY_ID === "minioadmin" ? "http://minio:9000" : 'https://83025b28472d6aa2bf5ae59f3724aa78.eu.r2.cloudflarestorage.com', 
+  endpoint:
+    env.R2_ACCESS_KEY_ID === "minioadmin"
+      ? "http://minio:9000"
+      : `https://${env.R2_ACCOUNT_ID}.eu.r2.cloudflarestorage.com`,
   credentials: {
     accessKeyId: env.R2_ACCESS_KEY_ID,
     secretAccessKey: env.R2_SECRET_KEY_ID,
