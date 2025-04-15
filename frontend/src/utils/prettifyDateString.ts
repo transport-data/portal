9779af80202f86689ddc1739f3029ba87bd481b3
Gutts-n <57202549+Gutts-n@users.noreply.github.com>
2025-04-15 00:19:59 +0000

    [xs][fix][private-dataset] Fixed error on private datasets that have start and end dates

--- frontend/src/utils/prettifyDateString.ts
+++ frontend/src/utils/prettifyDateString.ts
@@ -1,6 +1,10 @@
 export function prettifyDateString(date: string) {
   if (date.length > 10) {
-    return new Date(date);
+    return new Date(date).toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
   }
   const [year, month, day] = date.split("-").map(Number);
   const dateObject = new Date(year as number, (month as number) - 1, day);
